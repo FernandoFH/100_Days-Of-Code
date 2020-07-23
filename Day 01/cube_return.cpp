eb6712126210e82fbb7bca26b6aa661f75a72110
FernandoFH <ffhf.1820@gmail.com>
2020-07-23 11:01:24 -0300

    UPDATE: Day 01

---
+++ Day 01/cube_return.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+using namespace std;
+
+double cube(double num){
+    //double result = num * num * num;
+    //return result;
+
+    return num * num * num;
+}
+
+int main(){
+    //double answer = cube(5.0);
+    //cout << answer;
+
+    cout << cube(5.0);
+
+    return 0;
+}
