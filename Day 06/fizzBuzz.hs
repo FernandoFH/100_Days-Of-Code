144c6acf3187e36db507d6c473c643ee7cd36d6a
FernandoFH <ffhf.1820@gmail.com>
2022-09-03 22:47:53 -0300

    Iniciate FuzzBuzz

---
+++ Day 06/fizzBuzz.hs
@@ -0,0 +1,9 @@
+-- FizzBuzz rules:
+-- print numbers from 1 to 100
+-- if number is divisible by 3, print Fizz
+-- if number is divisible by 5, print Buzz
+-- if number is divisible by 3 and 5, print FizzBuzz
+-- otherwise, print the number
+
+module Main where
+
